9255cb63e7f27e62b196c76f9e9ee04ff191cc67
João Pedro Vargas <joao@fulano.dev>
2023-09-28 20:47:06 -0300

    Configurações Iniciais

---
+++ src/matchlove/matchlove/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace matchlove.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+    }
+}
