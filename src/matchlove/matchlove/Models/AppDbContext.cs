c77e79c5092d6b9831fc6fa1b23f2dd5d14a02d5
João Pedro Vargas <joao@fulano.dev>
2023-09-28 21:48:34 -0300

    Configurações Migrations e Database

--- src/matchlove/matchlove/Models/AppDbContext.cs
+++ src/matchlove/matchlove/Models/AppDbContext.cs
@@ -5,5 +5,6 @@ namespace matchlove.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+        public DbSet<Pessoa> Pessoas { get; set; }
     }
 }
--- src/matchlove/matchlove/Program.cs
+++ src/matchlove/matchlove/Program.cs
@@ -1,8 +1,13 @@
+using matchlove.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
