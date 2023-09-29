5bc8a02c094471a4ad42602311e5503e693d017b
João Pedro Vargas <joao@fulano.dev>
2023-09-29 20:01:12 -0300

    Teste curtida

---
+++ src/matchlove/matchlove/Models/Curtida.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace matchlove.Models
+{
+    [Table("Curtidas")]
+    public class Curtida
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdRemetente { get; set; }
+        public int IdDestinatario { get; set; }
+        public bool DeuMatch { get; set; }
+    }
+}
