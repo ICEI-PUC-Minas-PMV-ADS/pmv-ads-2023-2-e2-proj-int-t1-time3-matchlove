2d335210f4642ca90b7c30224ac5800b76983870
joaoabadia <128434710+joaoabadia@users.noreply.github.com>
2023-09-30 02:51:47 -0300

    Teste Denuncia

--- src/matchlove/matchlove/Models/AppDbContext.cs
+++ src/matchlove/matchlove/Models/AppDbContext.cs
@@ -6,5 +6,8 @@ namespace matchlove.Models
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
         public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Curtida> Curtida { get; set; }
+        public DbSet<Match> Match { get; set; }
+        public DbSet<Denuncia> Denuncia { get; set; }
     }
 }
---
+++ src/matchlove/matchlove/Models/Denuncia.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace matchlove.Models
+{
+    [Table("Denuncia")]
+    public class Denuncia
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Denunciante { get; set; }
+        public int Denunciado { get; set; }
+        public int Motivo { get; set; }
+        public string Descricao { get; set; }
+        public int Tipo { get; set; }
+        public ICollection<Pessoa> Pessoas { get; set; }
+
+        [ForeignKey("IdConteudo")]
+
+        public int IdConteudo { get; set; }
+    }
+
+}
