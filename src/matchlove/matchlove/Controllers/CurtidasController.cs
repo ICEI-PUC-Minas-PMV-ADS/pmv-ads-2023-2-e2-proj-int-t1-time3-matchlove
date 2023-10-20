ce122ec430a75585369bd4c2c52fed9fb8b6073d
joaoabadia <128434710+joaoabadia@users.noreply.github.com>
2023-10-20 13:15:16 -0300

    Criação do Controller das classes que estavam no Model (match, curtida, pessoa e denuncia)

---
+++ src/matchlove/matchlove/Controllers/CurtidasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using matchlove.Models;
+
+namespace matchlove.Controllers
+
+{
+    public class CurtidasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CurtidasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = _context.Curtida.ToList();
+            return View();
+        }
+    }
+}
