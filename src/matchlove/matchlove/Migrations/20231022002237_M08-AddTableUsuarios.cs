5cb878faca88a974c7976b1a89bfac3da1334f95
MRatton25 <114449344+MRatton25@users.noreply.github.com>
2023-10-22 12:58:57 -0300

    Login e autenticação

---
+++ src/matchlove/matchlove/Migrations/20231022002237_M08-AddTableUsuarios.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace matchlove.Migrations
+{
+    public partial class M08AddTableUsuarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuario",
+                table: "Usuario");
+
+            migrationBuilder.RenameTable(
+                name: "Usuario",
+                newName: "Usuarios");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "Usuario");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuario",
+                table: "Usuario",
+                column: "Id");
+        }
+    }
+}
